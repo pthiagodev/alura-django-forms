a3040751a88f098d61a393586204a2c304da5490
Thiago de Assis <pthiagodev@gmail.com>
2022-06-14 02:32:00 -0300

    Validando datas

--- passagens/validation.py
+++ passagens/validation.py
@@ -10,3 +10,13 @@ def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
     '''Verifica se possi algum dígito numérico'''
     if any(char.isdigit() for char in valor_campo):
             lista_de_erros[nome_campo] = 'Caracter inválido: não inclua números'
+
+def data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
+    '''Verifica se data de ida é maior que data de volta'''
+    if data_ida > data_volta:
+        lista_de_erros['data_volta'] = 'Data de volta não pode ser menor que a data de ida'
+
+def data_ida_menor_que_data_hoje(data_ida, data_pesquisa, lista_de_erros):
+    '''Verifica de data de ida é menor do que a data de hoje'''
+    if data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor que data de hoje'
